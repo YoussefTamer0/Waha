0c44052a00a25f054cd3b8d83da2ffc5ebc61ee2
judy <goudy74mostafa@gmail.com>
2026-04-25 14:33:09 +0300

    add validation

--- waha/src/main/java/com/bookstore/waha/model/OrderItem.java
+++ waha/src/main/java/com/bookstore/waha/model/OrderItem.java
@@ -2,6 +2,9 @@ package com.bookstore.waha.model;
 import jakarta.persistence.*;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 
@@ -9,8 +12,12 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotNull(message = "Book is required")
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
     private int quantity;
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
     private double price;
 
     @ManyToOne
